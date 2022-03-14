119f5f89a45bd6f0fc71640c8636dcb5d702e0a1
Luis Manzur <luismanzur91@outlook.com>
2022-03-14 11:39:03 -0400

    cancel y tiempo

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ pi = pigpio.pi()
 wiegand_decoder = wiegand.Decoder(pi, 14, 15, callback)
 wiegand_status = antenna.Status_Reader(14,15)
 wiegand_status.status()
+time.sleep(60)
+wiegand_decoder.cancel()
+
+
