a7f4cc3c49a0b22ef5b77db6c5d70bccbaed5835
Luis Manzur <luismanzur91@outlook.com>
2022-03-14 11:47:41 -0400

    anadido lector de caida

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import antenna
 
 
 def callback(bits, value):
-   card_id = int("{:026b}".format(value)[1:25],2)
-   print("Card ID: {:010d}".format(card_id))
+    card_id = int("{:026b}".format(value)[1:25],2)
+    if "{:010d}".format(card_id) == "0000000000":
+       print("Antena desconectada")
+    else:
+        print("Card ID: {:010d}".format(card_id))
 pi = pigpio.pi()
 wiegand_decoder = wiegand.Decoder(pi, 14, 15, callback)
-wiegand_status = antenna.Status_Reader(14,15)
-wiegand_status.status()
 time.sleep(60)
 wiegand_decoder.cancel()
 
