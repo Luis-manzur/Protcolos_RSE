98cf7d19e6ca153c08acc7081f8836182eaa5e8a
Luis Manzur <luismanzur91@outlook.com>
2022-03-14 12:20:55 -0400

    arreglando flujo de call back

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import antenna
 
 def callback(bits, value):
     card_id = int("{:026b}".format(value)[1:25],2)
-    if bits < 26:
-        print("Tierra No conectada")
-        wiegand_decoder.cancel()
-    elif "{:010d}".format(card_id) == "0000000000":
+    if "{:010d}".format(card_id) == "0000000000":
        print("Antena desconectada")
        wiegand_decoder.cancel()
+    elif bits < 26:
+        print("Tierra No conectada")
+        wiegand_decoder.cancel()
     else:
         print("Card ID: {:010d}".format(card_id))
 pi = pigpio.pi()
