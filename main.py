62d606f8f581e6caeb7ace1833876302fa96ae55
Luis Manzur <luismanzur91@outlook.com>
2022-03-14 12:08:50 -0400

    print el tipo de bits

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ import antenna
 
 def callback(bits, value):
     card_id = int("{:026b}".format(value)[1:25],2)
+    print(type(bits))
     if "{:010d}".format(card_id) == "0000000000":
        print("Antena desconectada")
+       wiegand_decoder.cancel()
     else:
         print("Card ID: {:010d}".format(card_id))
 pi = pigpio.pi()
